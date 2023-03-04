2dd0f7cc54612586c88ca722403f2bf8fb864698
Joao-code-spec <gaiagba@gmail.com>
2023-03-04 17:23:18 +0000

    reads input

---
+++ proj1/tsp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int maxVal;
+int main(int argc, char *argv[]){
+    FILE * file;
+    int totalCitys;
+    int totalRoads;
+    int i1, i2, distance;
+    if ((file = fopen(argv[1],"r")) == NULL){
+       printf("Error! file doesnt exist \n");
+       return 1;
+    }
+    fscanf(file,"%d %d", &totalCitys, &totalRoads);
+    int roadMatrix [totalCitys][totalCitys];
+    //printf("%d %d\n",totalCitys,totalRoads);
+    while(fscanf(file,"%d %d %d", &i1, &i2, &distance) != EOF){
+        roadMatrix[i1][i2]=distance;
+        roadMatrix[i2][i1]=distance;
+        //printf("%d %d %d\n", i1, i2, distance);
+    }
+    fclose(file);
+    maxVal = strtol(argv[2], NULL, 10);;
+    //printf("%d\n",maxVal);
+    return 0;
+}
