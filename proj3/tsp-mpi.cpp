149665484706671145ffd61f45022411d2967571
Joao-code-spec <gaiagba@gmail.com>
2023-04-08 19:18:25 +0100

    reshape works?

--- proj3/tsp-mpi.cpp
+++ proj3/tsp-mpi.cpp
@@ -229,7 +229,7 @@ bestTaC tspbb(std::vector<std::vector<double>> distances, int nCities, double be
         if(step%50==0){
             int y=queue.size();
             int x, z;
-            qElement eFromPrev={{1}, 2.3, 2.3, 2, 2};
+            qElement eFromPrev={{1,2,3,4,5,6}, 2.3, 2.3, 2, 2};
             MPI_Irecv(&x,1,MPI_INT,rankNext,5,MPI_COMM_WORLD,&reqForQL[2]);
             MPI_Irecv(&z,1,MPI_INT,rankPrev,4,MPI_COMM_WORLD,&reqForQL[3]);
 
@@ -241,7 +241,7 @@ bestTaC tspbb(std::vector<std::vector<double>> distances, int nCities, double be
 
             
             //Balance Sends one to next if nexts queue is shorter by 20
-            /*MPI_Waitall(4,reqForQL,statsForQL);
+            MPI_Waitall(4,reqForQL,statsForQL);
             if(x<y+20){
                 poppedE=queue.pop();
                 memcpy(myBalBuff, &poppedE.currentCity, sizeof(int));
@@ -266,7 +266,7 @@ bestTaC tspbb(std::vector<std::vector<double>> distances, int nCities, double be
                 eFromPrev.tour.resize(eFromPrev.lenght);
                 memcpy(eFromPrev.tour.data(),lnBalBuff+2*sizeof(int)+2*sizeof(double),eFromPrev.lenght*sizeof(int));
                 queue.push(eFromPrev);
-            }*/
+            }
 
             /*waits for reduce to finish and equalizes all bestTourCosts to the smallest*/
             MPI_Wait(&reqForReduce,MPI_STATUS_IGNORE);
